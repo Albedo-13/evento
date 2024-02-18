0b36a3de960172449a3e0a5125e8f8842747be35
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-02-18 14:25:47 +0300

    style: header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,34 @@
 import Link from "next/link";
 import Logo from "./logo";
 
+const routes = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "All Events",
+    href: "/events/all",
+  },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header
+      className="flex justify-between items-center 
+    border-b border-white/10 h-14 px-3
+    sm:px-9"
+    >
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All events</Link>
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((route) => (
+            <li key={route.name} className="text-white/50 hover:text-white transition">
+              <Link href={route.href}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
