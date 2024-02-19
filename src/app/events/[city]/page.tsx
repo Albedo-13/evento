e0379503abc79644c2bef11e1d79da5359f29cdc
PavelProkopenya <prokopenya.personal@gmail.com>
2024-02-19 14:43:36 +0300

    style: customize scrollbar

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,7 +2,7 @@ import H1 from "@/components/h1";
 
 export default function EventsPage() {
   return (
-    <main>
+    <main className="flex flex-col items-center px-5 py-24 min-h-[110vh]">
       <H1>Events in Austin</H1>
     </main>
   );
