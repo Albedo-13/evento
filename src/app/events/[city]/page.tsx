f59bc2c281ee8bf6b328903f33f52920d245268f
PavelProkopenya <prokopenya.personal@gmail.com>
2024-02-21 11:06:34 +0300

    feat: fetch events on server component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -6,12 +6,19 @@ type TProps = {
   };
 };
 
-export default function EventsPage({ params }: TProps) {
+export default async function EventsPage({ params }: TProps) {
   const city = params.city;
+
+  const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`);
+  const events = await response.json();
+  console.log(events);
 
   return (
     <main className="flex flex-col items-center px-5 py-24 min-h-[110vh]">
       <H1>{city === "all" ? "All Events" : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}</H1>
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
