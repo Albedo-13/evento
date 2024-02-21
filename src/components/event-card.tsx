8ea39e21b4f371f92d5a6052d86807f9bf1dbd8f
PavelProkopenya <prokopenya.personal@gmail.com>
2024-02-21 14:20:14 +0300

    style: minor style fix

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -7,8 +7,8 @@ type TProps = {
 
 export default function EventCard({ event }: TProps) {
   return (
-    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
-      <Image src={event.imageUrl} alt={event.name} width={500} height={280} className="h-[60%] object-fit"></Image>
+    <section className="flex flex-col flex-1 h-[380px] min-w-[320px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+      <Image src={event.imageUrl} alt={event.name} width={500} height={280} className="h-[60%] object-fit" />
       <div className="flex flex-col flex-1 justify-center items-center">
         <h2 className="text-2xl font-semibold">{event.name}</h2>
         <p className="italic text-white/75">By {event.organizerName}</p>
