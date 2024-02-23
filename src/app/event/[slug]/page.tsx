bffc647d39290c9f0a2d08cc5faac02936416ec5
PavelProkopenya <prokopenya.personal@gmail.com>
2024-02-23 13:04:48 +0300

    feat: fetch single event

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,16 @@
-export default function EventPage() {
-  return <main>EventPage</main>;
+import { TEvent } from "@/lib/types";
+
+type TProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: TProps) {
+  const slug = params.slug;
+  const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`);
+  const event: TEvent = await response.json();
+  console.log(event);
+
+  return <main>EventPage - {event.name}</main>;
 }
